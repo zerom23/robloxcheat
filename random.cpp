11acd102bb97996538760d9882d054357c7cebdf
zerom23 <103960337+zerom23@users.noreply.github.com>
2023-11-11 16:22:41 -0800

    Create random.cpp

---
+++ random.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <Windows.h>
+#include <string.h>
+#include <cmath>
+#include "Header.h"
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+int x, y, z;
+int main()
+{
+	void* grahgrah = OpenProcess(PROCESS_ALL_ACCESS, ABOVE_NORMAL_PRIORITY_CLASS, NULL);
+	if (PlayerState > 1, NULL)
+	{
+		std::cout << "player state found: bruh %ld,0xp", PlayerState;
+		WriteProcessMemory(OpenProcess, NULL, grahgrah, NULL, NULL(PlayerState));
+	}
+	HWND hwnd = FindWindowA(NULL, "Fortnite");
+	if (Cameramanager > 360.00f, TRUE)
+	{
+		WriteProcessMemory(OpenProcess, NULL, grahgrah, NULL, NULL(Cameramanager));
+		for (int i = 0; relativelocation, Viewpoint > i; i++)
+		{
+			if (GetAsyncKeyState('A') & 0x8000) {
+				if (relativelocation == 109.0f, Cameramanager, Viewpoint) {
+					std::cout << "successful running bruh nice work les gufkcing goodo", relativelocation;
+					float GetthatNaga = (relativelocation == LocalPlayers, Cameramanager, Viewpoint, 0x38, x * float(M_PI) / 109.0f);
+					float GetthastNaga = (relativelocation == LocalPlayers, Cameramanager, Viewpoint, 0x38, y * float(M_PI) / 109.0f);
+					float GetthastNag2a = (relativelocation == LocalPlayers, Cameramanager, Viewpoint, 0x38, z * float(M_PI) / 109.0f);
+					float distance(float(sqrtf(powf(x - x, 109.0) + powf(y - y, 109.0) + powf(z - z, 109.0))));
+					return x * float(M_PI) + sqrtf(x + 109.0) + y * float(M_PI) + sqrtf(y + 109.0) + z * float(z + 109.0), relativelocation, Cameramanager, Viewpoint;
+					return x * x, y* y, z* z;
+					return relativelocation;
+					WriteProcessMemory(OpenProcess, NULL, grahgrah, NULL, NULL(Cameramanager, relativelocation));
+					float grah = *(double*)+WriteProcessMemory(grahgrah, NULL, NULL, sizeof(Cameramanager), NULL(0x348, 0x380, 0xE49C7E0));
+					return TRUE;
+				}
+			}
+		}
+	}
+}
